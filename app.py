a2ddea60fb99db72bb03bfb6009261eee8c94bb5
Asad Chohan <asadchauhan906@gmail.com>
2023-03-29 16:16:01 +0500

    unit_test

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello mister: Why are you so cute'
+    return 'Hello World!'
 if __name__ == "__main__":
     app.run()
